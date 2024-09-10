a8404214efbfe18a080895e5cb706db6a8554c2f
David Cotton <dcotton@free.fr>
2024-09-10 20:50:17 +0200

    Init

--- src/test/java/ktest/domain/config/KTestConfigTest.java
+++ src/test/java/ktest/domain/config/KTestConfigTest.java
@@ -15,13 +15,13 @@ class KTestConfigTest {
     @Test
     void testContent() {
         final var environments = kConfig.environments();
-        assertEquals(4, environments.size());
+        assertEquals(2, environments.size());
         final var env = environments.getFirst();
         assertEquals("pi", env.name());
         assertEquals(4, env.onStartScript().size());
 
         final var brokers = kConfig.brokers();
-        assertEquals(5, brokers.size());
+        assertEquals(2, brokers.size());
         final var brk = kConfig.broker("pi_broker");
         assertEquals("pi_broker", brk.name());
         assertEquals("192.168.0.105:9092", brk.bootstrapServers());
@@ -33,7 +33,7 @@ class KTestConfigTest {
         assertEquals("ktest-group", brk.groupId());
 
         final var registries = kConfig.registries();
-        assertEquals(4, registries.size());
+        assertEquals(1, registries.size());
         final var reg = kConfig.registry("pi_registry");
         assertEquals("pi_registry", reg.name());
         assertEquals("http://192.168.0.105:8081", reg.url());
